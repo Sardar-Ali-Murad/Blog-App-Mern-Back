3fab64e1fd139b356c83e3f6e7faf7f0cd938f5b
your_username <sardaralimuradali4@gmail.com>
2023-04-26 09:23:10 +0500

    adding the admin url

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.use(
       "http://127.0.0.1:5174",
       "https://res.cloudinary.com",
       "https://blog-app-front.vercel.app",
+      "https://blog-app-mern-admin.vercel.app"
     ],
     // origin:"*"
   })
